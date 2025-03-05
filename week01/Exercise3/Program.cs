b3313c9962d5b04b0a3b1991092fa7ad33c65348
Taylor Allen <tjaroguetech@gmail.com>
2025-03-05 16:36:56 -0500

    Refactored the loop to allow player to restart once finished.  Game now keeps track of guess attempts.

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,28 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Random randomGenerator = new Random();
-        int magic_num = randomGenerator.Next(1, 100);
+        string playing;
 
-        Console.Write("Please try to guess the number: ");
-        int guess = int.Parse(Console.ReadLine());
+        Console.WriteLine("Welcome to the number guessing game!  How quickly can you guess the number?");
 
         do {
-            if (guess > magic_num)
-            {
-                Console.WriteLine("Lower");
-                Console.Write("Please try to guess the number: ");
-                guess = int.Parse(Console.ReadLine());
-            }
-            else if (guess < magic_num)
-            {
-                Console.WriteLine("Higher");
-                Console.Write("Please try to guess the number: ");
-                guess = int.Parse(Console.ReadLine());
-            }
-        } while (guess != magic_num);
+            Random randomGenerator = new Random();
+            int magic_num = randomGenerator.Next(1, 100);
+            int attempts = 0;
+            Console.WriteLine("A random number has been generated!");
 
-        Console.WriteLine("You guessed it!");
+            Console.Write("Please try to guess the number: ");
+            int guess = int.Parse(Console.ReadLine());
 
+            attempts++;
+
+            do {
+                if (guess > magic_num)
+                {
+                    Console.WriteLine("Lower");
+                    Console.Write("Please try to guess the number: ");
+                    guess = int.Parse(Console.ReadLine());
+                    attempts++;
+                }
+                else if (guess < magic_num)
+                {
+                    Console.WriteLine("Higher");
+                    Console.Write("Please try to guess the number: ");
+                    guess = int.Parse(Console.ReadLine());
+                    attempts++;
+                }
+            } while (guess != magic_num);
+
+            Console.WriteLine($"You guessed it! It took you {attempts} guesses.");
+            Console.Write("Would you like to play again? (yes or no): ");
+            playing = Console.ReadLine();
+
+            } while (playing == "yes");
     }
 }
