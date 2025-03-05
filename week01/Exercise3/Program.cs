12b00301a0430e05ad9e5a07f7473fcf2205cae8
Taylor Allen <tjaroguetech@gmail.com>
2025-03-05 16:23:15 -0500

    Initial commit.  Added base requirement solution with simple loop and random number generator.

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magic_num = randomGenerator.Next(1, 100);
+
+        Console.Write("Please try to guess the number: ");
+        int guess = int.Parse(Console.ReadLine());
+
+        do {
+            if (guess > magic_num)
+            {
+                Console.WriteLine("Lower");
+                Console.Write("Please try to guess the number: ");
+                guess = int.Parse(Console.ReadLine());
+            }
+            else if (guess < magic_num)
+            {
+                Console.WriteLine("Higher");
+                Console.Write("Please try to guess the number: ");
+                guess = int.Parse(Console.ReadLine());
+            }
+        } while (guess != magic_num);
+
+        Console.WriteLine("You guessed it!");
+
     }
 }
