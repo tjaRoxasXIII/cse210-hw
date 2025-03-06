d1699df7a6c62fe4baf1d16e6c88d6b9101494a7
unknown <tjaroguetech@gmail.com>
2025-03-05 20:29:16 -0500

    Solving base criteria.  Program creates a list of integers and reports back the list's sum, average, and largest value.

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> num_list = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int number; 
+
+        do 
+        {
+            Console.Write("Enter a number: ");
+            number = int.Parse(Console.ReadLine());
+            if (number != 0)
+            {
+                num_list.Add(number);
+            }
+
+        } while (number != 0);
+
+        foreach (int num in num_list)
+        {
+            Console.WriteLine(num);
+        }
+        
+        Console.WriteLine($"The sum of the list is: {num_list.Sum()}");
+        Console.WriteLine($"The average is: {num_list.Average()}");
+        Console.WriteLine($"The largest number is: {num_list.Max()}");
     }
 }
