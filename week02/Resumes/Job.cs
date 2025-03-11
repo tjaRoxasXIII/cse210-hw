e0e47ac7e3384b75d20446f9fb59285e2906d7d3
unknown <tjaroguetech@gmail.com>
2025-03-10 20:18:19 -0400

    Creating Job class and testing invocation in Program.cs

---
+++ week02/Resumes/Job.cs
@@ -0,0 +1,12 @@
+public class Job
+{
+    public string _company;
+    public string _jobTitle;
+    public int _startyear;
+    public int _endyear;
+
+    static void Display()
+    {
+
+    }
+}
--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -5,5 +5,21 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startyear = 1990;
+        job1._endyear = 2007;
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Software Architect";
+        job2._startyear = 2000;
+        job2._endyear = 2015;
+
+        Console.WriteLine(job1._company);
+        Console.WriteLine(job2._company);
     }
+
+
 }
