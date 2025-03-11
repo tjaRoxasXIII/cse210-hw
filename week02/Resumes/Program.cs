24f2017f2be3b6a1667c5d455d5678ba10dfaa00
unknown <tjaroguetech@gmail.com>
2025-03-10 20:46:11 -0400

    Removing Sample Code

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,7 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
         Job job1 = new Job();
         job1._company = "Microsoft";
         job1._jobTitle = "Software Engineer";
