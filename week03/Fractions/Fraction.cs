1e3a5cf853b22edc8681e8f1257b382231361a38
unknown <tjaroguetech@gmail.com>
2025-03-16 23:23:35 -0400

    Adding Getter/Setter functions.

--- week03/Fractions/Fraction.cs
+++ week03/Fractions/Fraction.cs
@@ -21,4 +21,24 @@ class Fraction
         _bottom = bottom;
     }
 
+    public int GetTop()
+    {
+        return _top;
+    }
+
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+
 }
