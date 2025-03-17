63f35c0a14842c0e5ce18e9235a09acdef2abb2f
unknown <tjaroguetech@gmail.com>
2025-03-16 23:17:23 -0400

    Creating constructors and verifying that instances are created.

--- week03/Fractions/Fraction.cs
+++ week03/Fractions/Fraction.cs
@@ -3,6 +3,22 @@ class Fraction
     private int _top;
     private int _bottom;
 
-   
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int top)
+    {
+        _top = top;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
 
 }
--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction fraction1 = new Fraction();
+        Fraction fraction2 = new Fraction(6);
+        Fraction fraction3 = new Fraction(6, 7);
+        Console.WriteLine($"{fraction1}");
+        Console.WriteLine($"{fraction2}");
+        Console.WriteLine($"{fraction3}");
     }
 }
