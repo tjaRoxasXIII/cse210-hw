9e7a4fc0074fcf24933388d983bf0d8f1b652edb
unknown <tjaroguetech@gmail.com>
2025-03-16 23:39:18 -0400

    Removing initial test code

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,13 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Fraction fraction1 = new Fraction();
-        Fraction fraction2 = new Fraction(6);
-        Fraction fraction3 = new Fraction(6, 7);
-        Console.WriteLine($"{fraction1}");
-        Console.WriteLine($"{fraction2}");
-        Console.WriteLine($"{fraction3}");
-
         Console.WriteLine("Writing sample outputs:");
         Console.WriteLine(new Fraction().GetFractionString());
         Console.WriteLine(new Fraction().GetDecimalValue());
