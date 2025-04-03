41f18196a3a356f0f979b8047488e6722e559e23
unknown <tjaroguetech@gmail.com>
2025-04-03 01:50:19 -0400

    Solution Complete

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Reflection;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment test = new Assignment("Sam Jackson", "Mathematics");
+        MathAssignment mathTest = new MathAssignment("Jack Johnson", "Trigonemtry", "7", "1-23");
+        WritingAssignment writingTest = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II by Mary Waters");
+
+        Console.WriteLine(test.GetSummary());
+
+        Console.WriteLine(mathTest.GetSummary());
+        Console.WriteLine(mathTest.GetHomeworkList());
+
+        Console.WriteLine(writingTest.GetSummary());
+        Console.WriteLine(writingTest.GetWritingInformation());
+
     }
+    
 }
+
