2064c8155deb5f1436ae2d116231ba4d31de8740
unknown <tjaroguetech@gmail.com>
2025-04-04 23:54:35 -0400

    Adding features for Reflection Activity.  Still lacking user input.

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -2,10 +2,32 @@ using System;
 
 class Program
 {
+        
     static void Main(string[] args)
     {
+        List<string> prompts = [
+            "Think of a time when you stood up for someone else.",
+            "Think of a time when you did something really difficult.",
+            "Think of a time when you helped someone in need.",
+            "Think of a time when you did something truly selfless."
+            ];
+    
+        List<string> questions = [
+            "Why was this experience meaningful to you?",
+            "Have you ever done anything like this before?",
+            "How did you get started?",
+            "How did you feel when it was complete?",
+            "What made this time different than other times when you were not as successful?",
+            "What is your favorite thing about this experience?",
+            "What could you learn from this experience that applies to other situations?",
+            "What did you learn about yourself through this experience?",
+            "How can you keep this experience in mind in the future?"
+            ];
+
+
         Console.WriteLine("Hello World! This is the Mindfulness Project.");
-        BreathingActivity test = new BreathingActivity();
+        //BreathingActivity test = new BreathingActivity();
+        ReflectingActivity test = new ReflectingActivity(prompts, questions);
 
         //test.ShowSpinner(30);
         //test.ShowCountDown(5);
--- week05/Mindfulness/ReflectingActivity.cs
+++ week05/Mindfulness/ReflectingActivity.cs
@@ -3,7 +3,7 @@ class ReflectingActivity : Activity
     List<string> _prompts;
     List<string> _questions;
 
-    public ReflectingActivity(List<string> prompts, List<string> questions) :base("name", "description", 30)
+    public ReflectingActivity(List<string> prompts, List<string> questions) :base("Reflection", "This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.", 30)
     {
         _prompts = prompts;
         _questions = questions;
@@ -11,7 +11,34 @@ class ReflectingActivity : Activity
 
     public void Run()
     {
+        DisplayStartingMessage();
 
+        Random rnd = new Random();
+        int r_prompt = rnd.Next(_prompts.Count);
+        Console.WriteLine(_prompts[r_prompt]);
+        Console.WriteLine();
+        Thread.Sleep(3000);
+        
+        DateTime startTime = DateTime.Now;
+        DateTime futureTime = startTime.AddSeconds(base.GetDuration());
+
+        DateTime currentTime = DateTime.Now;
+        while(currentTime < futureTime)
+        {
+            int r_question = rnd.Next(_questions.Count);
+            Console.WriteLine(_questions[r_question]);
+            ShowSpinner(15);
+            Console.Clear();
+
+            Console.WriteLine(_prompts[r_prompt]);
+            Console.WriteLine();
+
+            currentTime = DateTime.Now;
+
+        }
+        
+        Console.Clear();
+        DisplayEndingMessage();
     }
 
     public string GetRandomPrompt()
