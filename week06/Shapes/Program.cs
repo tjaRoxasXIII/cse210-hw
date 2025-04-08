240d14520ac363418e66f897cba33ed37a93551a
Taylor Allen <tjaroguetech@gmail.com>
2025-04-08 10:11:16 -0400

    Uploading solution

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+
+        List<Shape> shapes = [new Square("Red", 10), new Rectangle("Blue", 15, 10), new Circle("Green", 21)];
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+            Console.WriteLine("--------");
+        }
     }
 }
